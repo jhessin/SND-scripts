bc84ab35a3e16b2350f8ffc783bfa50ddd0418e6
Jim Hessin <jhessin@gmail.com>
2024-11-10 21:20:28 -0700

    Sun Nov 10 21:20:31 2024

--- GearUpdater.lua
+++ GearUpdater.lua
@@ -1,3 +1,21 @@
+--[[
+--  This script will go through a set number of gearsets and update them with
+--  the recommended gear. It requires autoduty and Simple Tweaks setting
+--  Gearset Update Command
+--]]
+--
+-- Options:
+-- This is the total number of gearsets to update This way you aren't updating
+-- specialty gearsets.
+totalGearsets = 34
+-- totalGearsets = 2
+
+-- This is the command that is set up as your "Gearset Update Command" in
+-- simple tweaks.
+updateCommand = "/updategearset"
+
+-- Start script --
+
 SNDConfigFolder = os.getenv("appdata") .. "\\XIVLauncher\\pluginConfigs\\SomethingNeedDoing\\"
 LoadFunctionsFileLocation = SNDConfigFolder .. "vac_functions.lua"
 LoadFunctions = loadfile(LoadFunctionsFileLocation)
@@ -9,12 +27,11 @@ if not CheckPluginsEnabled("AutoDuty") then
 end
 
 yield("/echo Updating Gearsets please wait...")
-totalGearsets = 34
 
 for i=1,totalGearsets do
   yield("/gearset change " .. tostring(i))
   yield("/ad equiprec")
-  yield("/updategearset")
+  yield(updateCommand)
   yield("/wait 5")
 end
 yield("/echo Finished Updating Gear!")
